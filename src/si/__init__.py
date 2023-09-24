68ee5cadb80348889a1267a550d5592f60359ac7
rawcode22 <120012181+rawcode22@users.noreply.github.com>
2023-09-24 14:58:16 +0100

    Update __init__.py

--- src/si/__init__.py
+++ src/si/__init__.py
@@ -2,8 +2,8 @@ __author__ = "João Correia"
 __credits__ = ["João Correia, Fernando Cruz", "Vítor Pereira"]
 __license__ = "Apache License 2.0"
 __version__ = "0.0.1"
-__maintainer__ = "João Correia"
-__email__ = "jfscorreia95@gmail.com"
+__maintainer__ = "José Lemos"
+__email__ = "zemarialemos@gmail.com"
 
 # -----------------------------------------------------------------------------------
 # This package is heavily inspired and adapted from
