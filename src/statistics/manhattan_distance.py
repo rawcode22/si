648100dd2f82f9825471f693ce492c772c626512
rawcode22 <120012181+rawcode22@users.noreply.github.com>
2023-12-18 01:45:04 +0000

    Add files via upload

---
+++ src/statistics/manhattan_distance.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+def manhattan_distance(x: np.ndarray, y:np.ndarray) -> np.ndarray:
+    '''
+    this function calculates the manhattan distance of a point x to a group of y points.
+    
+        xtoy1 = |x1 - y11| + |x2 - y12| + ... + |xn - y1n|
+        xtoy2 = |x1 - y21| + |x2 - y22| + ... + |xn - y2n|
+        
+    Parameters:
+    x -> Point.
+    y -> Set of points.
+
+    Returns:
+    np.ndarray ->  Manhattan distance for each point in y.
+    '''
+    return np.abs((x-y).sum(axis=1))
